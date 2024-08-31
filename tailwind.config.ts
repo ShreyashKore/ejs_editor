7112b0f83b163c99bfb9c1ef9cc5ce10bf34198d
Shreyash Kore <kore.shreyash@gmail.com>
2024-09-01 02:49:36 +0530

    setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,70 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
+const colors = {
+  primary: {
+    DEFAULT: '#8c4c89',
+    light: '#8c4c89',
+    dark: '#303f9f',
+  },
+  onPrimary: {
+    DEFAULT: '#000000',
+    light: '#000000',
+    dark: '#ffffff',
+  },
+  secondary: {
+    DEFAULT: '#ff4081',
+    light: '#ff4081',
+    dark: '#c51162',
+  },
+  onSecondary: {
+    DEFAULT: '#000000',
+    light: '#000000',
+    dark: '#ffffff',
+  },
+  surface: {
+    DEFAULT: '#ff4081',
+    light: '#ff4081',
+    dark: '#c51162',
+  },
+  onSurface: {
+    DEFAULT: '#000000',
+    light: '#000000',
+    dark: '#ffffff',
+  },
+  background: {
+    DEFAULT: '#ff4081',
+    light: '#ff4081',
+    dark: '#c51162',
+  },
+  onBackground: {
+    DEFAULT: '#000000',
+    light: '#000000',
+    dark: '#ffffff',
+  },
+  error: {
+    DEFAULT: '#e57373',
+    light: '#e57373',
+    dark: '#d32f2f',
+  },
+  warning: {
+    DEFAULT: '#ffb74d',
+    light: '#ffb74d',
+    dark: '#f57c00',
+  },
+  info: {
+    DEFAULT: '#64b5f6',
+    light: '#64b5f6',
+    dark: '#1976d2',
+  },
+  success: {
+    DEFAULT: '#81c784',
+    light: '#81c784',
+    dark: '#388e3c',
+  },
+};
+
+
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
@@ -8,6 +72,7 @@ export default {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
+      colors: colors
     },
   },
   plugins: [],
